9cc43cb7d2d678f79c36a38d820b6254266cfbe3
M.Q <maryamqasemi2022@gmail.com>
2024-02-22 02:28:33 +0330

    install react-toastify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,29 @@
 import Header from "@/components/Header/Header";
 import "bootstrap/dist/css/bootstrap.css";
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
-
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import "@/styles/globals.css";
+import axios from "axios";
+import https from "https";
+import { AuthProvider } from "@/contexts/AuthContext";
+if (process.env.NODE_ENV === "development") {
+  axios.defaults.httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+  });
+}
 const App = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
     import("bootstrap");
   }, []);
+
   return (
-    <>
+    <AuthProvider>
       <Header />
       <Component {...pageProps} />
-    </>
+      <ToastContainer />
+    </AuthProvider>
   );
 };
 
